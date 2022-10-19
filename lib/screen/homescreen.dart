be4d47adff149752c28bf3cb3a8309494cf6a134
Muhammed Nihal Cm <84779705+Nihalcm@users.noreply.github.com>
2022-10-19 10:52:11 +0530

    futurebuilder
    added futurebuilder

--- lib/screen/homescreen.dart
+++ lib/screen/homescreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:newstoday/modelclass/news.dart';
 import 'package:newstoday/screen/Detailednews.dart';
+import 'package:newstoday/service/apiservice.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -9,6 +11,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  ApiService client=ApiService();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,84 +27,100 @@ class _HomeScreenState extends State<HomeScreen> {
                 fontFamily: 'RobotoSlab',
                 fontWeight: FontWeight.bold),
           )),
-      body: ListView.builder(
-          itemCount: 4,
-          itemBuilder: (context, index) {
-            return InkWell(onTap: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) => DetailedScreen(),));
-            },
-              child: Container(
-                margin: EdgeInsets.only(left: 16, right: 16, top: 12, bottom: 12),
-                child: ClipRRect(
-                    borderRadius: BorderRadius.circular(15),
-                    child: Stack(
-                      children: [
-                        SizedBox(
-                          height: 200,
-                          width: 400,
-                          child: Image.network(
-                            'https://i.insider.com/62e8eef760b200001871e742?width=1136&format=jpeg',
-                            fit: BoxFit.fill,
-                          ),
-                        ),SizedBox(width: 400,height: 200,child: DecoratedBox(decoration:BoxDecoration(
-                gradient: LinearGradient(
-                colors: [Colors.transparent,
-                Colors.black45,
-                    Colors.black
-                    ],
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter)),),),
-                        Positioned(
-                            bottom: 0,
-                            child: Container(
-                                width: 400,
+      body: FutureBuilder<List<News>>(future: client.fetchNews(),builder: (BuildContext,AsyncSnapshot<List<News>> snapshot) {
+        return ListView.builder(
+            itemCount: 4,
+            itemBuilder: (context, index) {
+              return InkWell(onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => DetailedScreen(),));
+              },
+                child: Container(
+                  margin: EdgeInsets.only(
+                      left: 16, right: 16, top: 12, bottom: 12),
+                  child: ClipRRect(
+                      borderRadius: BorderRadius.circular(15),
+                      child: Stack(
+                        children: [
+                          SizedBox(
+                            height: 200,
+                            width: 400,
+                            child: Image.network(
+                              'https://i.insider.com/62e8eef760b200001871e742?width=1136&format=jpeg',
+                              fit: BoxFit.fill,
+                            ),
+                          ), SizedBox(width: 400,
+                            height: 200,
+                            child: DecoratedBox(decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                    colors: [Colors.transparent,
+                                      Colors.black45,
+                                      Colors.black
+                                    ],
+                                    begin: Alignment.topCenter,
+                                    end: Alignment.bottomCenter)),),),
+                          Positioned(
+                              bottom: 0,
+                              child: Container(
+                                  width: 400,
 
-                                child: Column(
-                                  children: [
-                                    Align(
-                                        alignment: Alignment.topLeft,
+                                  child: Column(
+                                    children: [
+                                      Align(
+                                          alignment: Alignment.topLeft,
+                                          child: Container(
+                                            margin: EdgeInsets.only(left: 12),
+                                            width: 300,
+                                            child: Text(
+                                              'My Name Is Muhammed Nihal cm.Iam a Flutter Developer',
+                                              style: TextStyle(
+                                                  fontFamily: 'RobotoSlab',
+                                                  fontSize: 20,
+                                                  color: Color(0xFFF2F2F2)),
+                                            ),
+                                          )),
+                                      Align(alignment: Alignment.topLeft,
                                         child: Container(
-                                          margin: EdgeInsets.only(left: 12),
-                                          width: 300,
-                                          child: Text(
-                                            'My Name Is Muhammed Nihal cm.Iam a Flutter Developer',
-                                            style: TextStyle(
-                                                fontFamily: 'RobotoSlab',
-                                                fontSize: 20,
-                                                color: Color(0xFFF2F2F2)),
+                                          margin: EdgeInsets.only(
+                                            bottom: 12,
+                                            top: 19,
+                                            left: 12,
                                           ),
-                                        )),
-                                    Align(alignment: Alignment.topLeft,
-                                      child: Container(
-                                        margin: EdgeInsets.only(
-                                          bottom: 12,
-                                          top: 19,
-                                          left: 12,
-                                        ),
-                                        child: Row(mainAxisAlignment: MainAxisAlignment.start,
-                                          children: [ Text(
-                                            'flutter',
-                                            style: TextStyle(fontFamily: 'RobotoSlab',fontWeight: FontWeight.bold,
-                                                fontSize: 12,
-                                                color: Color(0xFFBABABA)),
-                                          ),
-                                            Padding(padding: EdgeInsets.only(left: 10),
-                                              child: Text(
-                                                '2019/09/09',
-                                                style: TextStyle(fontFamily: 'RobotoSlab',
+                                          child: Row(
+                                              mainAxisAlignment: MainAxisAlignment
+                                                  .start,
+                                              children: [ Text(
+                                                'flutter',
+                                                style: TextStyle(
+                                                    fontFamily: 'RobotoSlab',
+                                                    fontWeight: FontWeight.bold,
                                                     fontSize: 12,
                                                     color: Color(0xFFBABABA)),
                                               ),
-                                            )]),
-                                      ),
-                                    )
-                                  ],
-                                )))
-                      ],
-                    )),
-              ),
-            );
-          }),
+                                                Padding(
+                                                  padding: EdgeInsets.only(
+                                                      left: 10),
+                                                  child: Text(
+                                                    '2019/09/09',
+                                                    style: TextStyle(
+                                                        fontFamily: 'RobotoSlab',
+                                                        fontSize: 12,
+                                                        color: Color(
+                                                            0xFFBABABA)),
+                                                  ),
+                                                )
+                                              ]),
+                                        ),
+                                      )
+                                    ],
+                                  )))
+                        ],
+                      )),
+                ),
+              );
+            });
+
+      }),
     );
   }
 }
